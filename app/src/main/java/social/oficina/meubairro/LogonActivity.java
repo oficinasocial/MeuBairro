52e03edcf777f45ae8d94cfe260bd3f8a9c3acaf
Gabriel Oliveira <gabriel.poa@gmail.com>
2016-06-14 22:47:37 -0300

    Initial commit

---
+++ app/src/main/java/social/oficina/meubairro/LogonActivity.java
@@ -0,0 +1,29 @@
+package social.oficina.meubairro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.facebook.FacebookSdk;
+
+public class LogonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_logon);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_logon);
+    }
+
+
+}
